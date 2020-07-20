15df82986110894ecde830951e80afc6533df53a
Mingding, Han <hanmd82@gmail.com>
2020-07-20 17:14:54 +0800

    feat: Update showGist handler to accept a query param `id`

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -13,8 +15,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from GoGists"))
 }
 
-func showGist(w http.ResponseWriter, _ *http.Request) {
-	w.Write([]byte("Display a specific gist..."))
+func showGist(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Show a specific gist with ID %d...", id)
 }
 
 func createGist(w http.ResponseWriter, r *http.Request) {
