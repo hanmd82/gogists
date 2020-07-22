49fa882f99b48942bd7c3cb1d6cb774bd0740e04
Mingding, Han <hanmd82@gmail.com>
2020-07-22 11:16:20 +0800

    refactor: Extract error handling code into helper methods

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -22,22 +22,20 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// Read the template files into a template set
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError) // 500
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError) // 500
+		app.serverError(w, err)
 	}
 }
 
 func (app *application) showGist(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -47,7 +45,7 @@ func (app *application) showGist(w http.ResponseWriter, r *http.Request) {
 func (app *application) createGist(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed) // 405
+		app.clientError(w, http.StatusMethodNotAllowed) // 405
 		return
 	}
 	w.Write([]byte("Create a new gist..."))
