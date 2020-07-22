02be0f9c9612ec93447c66b5d375977eb93935e2
Mingding, Han <hanmd82@gmail.com>
2020-07-22 09:43:34 +0800

    feat: Use leveled logging for INFO and ERROR logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,15 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// Start a new web server and register functions as handlers for respective URL patterns.
 	mux := http.NewServeMux()
@@ -19,8 +23,8 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
--- doc/chapter3.md
+++ doc/chapter3.md
@@ -1,1 +1,7 @@
-- Use command-line flags to manage configuration settings
+- Use command-line flags to manage configuration settings.
+- Leveled Logging
+  - Prefix informational messages with "INFO" and output to stdout.
+  - Prefix error messages with "ERROR" and output to stderr, along with the relevant file name and line number (for debugging).
+    ```bash
+    $ go run ./cmd/web >>/tmp/info.log 2>>/tmp/error.log
+    ```
