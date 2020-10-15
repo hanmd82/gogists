c1ef12280d0cf6fbbd9448107c88e7166ab5d606
Mingding, Han <hanmd82@gmail.com>
2020-10-15 10:55:47 +0800

    feat: Add middleware to secure HTTP headers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	// Create a new web server and register methods as handlers for respective URL patterns.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
@@ -12,5 +12,5 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return mux
+	return secureHeaders(mux)
 }
