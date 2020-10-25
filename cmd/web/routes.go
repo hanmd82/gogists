0842964ad6fd084854218a12523f832c774469a9
Mingding, Han <hanmd82@gmail.com>
2020-10-25 23:42:50 +0800

    feat: Setup links, routes and handlers with placeholder logic

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,12 @@ func (app *application) routes() http.Handler {
 	mux.Post("/gists/create", dynamicMiddleware.ThenFunc(app.createGist))
 	mux.Get("/gist/:id", dynamicMiddleware.ThenFunc(app.showGist))
 
+	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
+	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
+	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
+	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
+	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser))
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
---
+++ doc/chapter11-user-authentication.md
@@ -0,0 +1,10 @@
+## User Authentication
+
+Goals
+- How to implement basic signup, login and logout functionality for users
+- Encrypt and store user passwords securely in database using bcrypt
+- Verify that a user is logged in using middleware and sessions
+- How to prevent Cross-Site Request Forgery (CSRF) attacks
+
+---
+
