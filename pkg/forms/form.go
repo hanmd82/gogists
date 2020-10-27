3317fcb04d95ddd61028f248af30214a2e1fd802
Mingding, Han <hanmd82@gmail.com>
2020-10-27 11:07:06 +0800

    feat: Add form validation for User signups

--- pkg/forms/form.go
+++ pkg/forms/form.go
@@ -5,12 +5,15 @@ import (
 	"net/url"
 	"strings"
 	"unicode/utf8"
+	"regexp"
 )
 
 type Form struct {
 	url.Values
 	Errors errors
 }
+
+var EmailRgx = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func New(data url.Values) *Form {
 	return &Form{
@@ -25,6 +28,16 @@ func (f *Form) Required(fields ...string) {
 		if strings.TrimSpace(value) == "" {
 			f.Errors.Add(field, "This field cannot be blank")
 		}
+	}
+}
+
+func (f *Form) MinLength(field string, d int) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+	if utf8.RuneCountInString(value) < d {
+		f.Errors.Add(field, fmt.Sprintf("This field is too short (minimum is %d characters)", d))
 	}
 }
 
@@ -50,6 +63,16 @@ func (f *Form) PermittedValues(field string, opts ...string) {
 		}
 	}
 	f.Errors.Add(field, "This field is invalid")
+}
+
+func (f *Form) MatchesPattern(field string, pattern *regexp.Regexp) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+	if !pattern.MatchString(value) {
+		f.Errors.Add(field, "This field is invalid")
+	}
 }
 
 func (f *Form) Valid() bool {
